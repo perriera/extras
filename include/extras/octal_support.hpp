5162601f4cbac2d9817381447d498f81d2ea978f
Perry Anderson <perry.anderson@gmail.com>
2021-08-10 22:46:49 -0700

    Added OctalInterface

---
+++ include/extras/octal_support.hpp
@@ -0,0 +1,23 @@
+#ifndef _EXTRA_OCTAL_SUPPORT_HPP
+#define _EXTRA_OCTAL_SUPPORT_HPP
+
+#include <extra/interfaces.hpp>
+#include <iostream>
+
+namespace extras {
+
+/**
+ * @brief OctalInterface
+ * 
+ * Used by the ChessMind project, it introduces an interface
+ * to safely convert a number to and from octal format.
+ * 
+ */
+
+interface OctalInterface {
+  virtual int toOctal() const = 0;
+  virtual void fromOctal(int octal) = 0;
+};
+
+
+#endif // _EXTRA_OCTAL_SUPPORT_HPP
