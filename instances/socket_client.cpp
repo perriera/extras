0c2a2faf3dc0f94e3fb0d43efb4b25d50c2cd018
Perry Anderson <perry.anderson@gmail.com>
2021-10-17 00:13:32 -0700

    Updated code

--- instances/socket_client.cpp
+++ instances/socket_client.cpp
@@ -7,10 +7,14 @@ using namespace extras;
 
 #define PORT 8000
 int main(int, char const*[]) {
-  SocketClient client("127.0.0.1", PORT);
-  client.connect();
-  client.send("Hello from SocketClient");
-  client.read();
-  string msg = client;
-  cout << msg << endl;
+  try {
+    SocketClient client("127.0.0.1", PORT);
+    client.connect();
+    client.send("Hello from SocketClient");
+    client.read();
+    string msg = client;
+    cout << msg << endl;
+  } catch (SocketException& ex) {
+    cout << ex.what() << endl;
+  }
 }
--- instances/socket_server.cpp
+++ instances/socket_server.cpp
@@ -7,10 +7,14 @@ using namespace extras;
 
 #define PORT 8000
 int main(int, char const*[]) {
-  SocketServer server(PORT);
-  server.accept();
-  server.read();
-  string msg = server;
-  cout << msg << endl;
-  server.send("Hello from SocketServer");
+  try {
+    SocketServer server(PORT);
+    server.accept();
+    server.read();
+    string msg = server;
+    cout << msg << endl;
+    server.send("Hello from SocketServer");
+  } catch (SocketException& ex) {
+    cout << ex.what() << endl;
+  }
 }
