d41ed5448ef7911353931535eb4e503878086757
Perry Anderson <perry.anderson@gmail.com>
2021-10-19 20:58:36 -0700

    Added test case

--- test/mock_filestransfer.cpp
+++ test/mock_filestransfer.cpp
@@ -1,8 +1,15 @@
+#include <extras/bin2hex/BinFile.hpp>
+#include <extras/bin2hex/HexConverter.hpp>
+#include <extras/bin2hex/HexFile.hpp>
+#include <extras/bin2hex/HexFileTransfer.hpp>
+#include <extras/sockets/SocketClient.hpp>
+#include <extras/sockets/SocketServer.hpp>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
 #include "catch.hpp"
-#include "extras/bin2hex/HexFile.hpp"
-#include "extras/bin2hex/HexFileTransfer.hpp"
 #include "fakeit.hpp"
 
 //
@@ -11,13 +18,25 @@
 
 using namespace extras;
 using namespace fakeit;
+namespace fs = std::filesystem;
 
 /**
  * @brief Mock FakeIt AlwaysDo demonstration
  *
  */
 SCENARIO("Mock HexFileTransferInterface: transfer", "[HexFileTransfer]") {
-  HexFile hexFile;
+  std::string filename = "data/Downloads/cplusplusorg.freeformjs.imploded.zip";
+  std::ifstream myfile(filename);
+  REQUIRE(myfile.good());
+  BinFile binFile;
+  myfile >> binFile;
+  auto file_size = fs::file_size(filename);
+  REQUIRE(binFile.array() != nullptr);
+  REQUIRE(binFile.size() == file_size);
+  HexConverter hexConverter;
+  HexFile hexFile = hexConverter.bin2hex(binFile);
+  REQUIRE(hexFile.size() == file_size * 2);
+
   Mock<HexFileTransferInterface> mock;
   When(Method(mock, transfer)).Return();
 
