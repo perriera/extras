d8e0a60ba2cd6ff4626c07b790a5b3ca78ef4947
Perry Anderson <perry.anderson@gmail.com>
2021-10-20 13:50:50 -0700

    Added mock

---
+++ test/mock_temperatuers.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+
+#include "catch.hpp"
+#include "extras/devices/AverageTemp.hpp"
+#include "fakeit.hpp"
+
+//
+// https://github.com/eranpeer/FakeIt/wiki/Quickstart
+//
+
+using namespace extras;
+using namespace fakeit;
+
+SCENARIO("Mock AverageTempInterface: array/size", "[AverageTempInterface]") {
+  const float correct_answer = 50;
+  Mock<AverageTempInterface> mock;
+  When(Method(mock, addTemperature)).Return();
+  When(Method(mock, calculateTemperature)).Return(correct_answer);
+  When(Method(mock, lastCalculatedTemperature)).Return(correct_answer);
+
+  AverageTempInterface& i = mock.get();
+  i.addTemperature(correct_answer);
+  REQUIRE(i.calculateTemperature() == correct_answer);
+  REQUIRE(i.lastCalculatedTemperature() == correct_answer);
+  Verify(Method(mock, addTemperature));
+  Verify(Method(mock, calculateTemperature));
+  Verify(Method(mock, lastCalculatedTemperature));
+}
