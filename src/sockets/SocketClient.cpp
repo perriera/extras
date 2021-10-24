1e0ed19f29ff8b604c4312366393c57f6fe9b6ae
Perry Anderson <perry.anderson@gmail.com>
2021-10-24 02:30:00 -0700

    Fixed issue

--- src/sockets/SocketClient.cpp
+++ src/sockets/SocketClient.cpp
@@ -8,7 +8,6 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-#include <cctype>
 #include <extras/sockets/Socket.hpp>
 #include <iostream>
 #include <sstream>
@@ -18,48 +17,23 @@ using namespace std;
 namespace extras {
 
   SocketClient::SocketClient(const std::string &hostname, int port)
-      : _hostname(hostname), _port(port) {}
-
-  // void SocketClient::connect() {
-  //   this->_socket = socket(AF_INET, SOCK_STREAM, 0);
-  //   SocketException::assertLTZ(_socket, "SocketClient creation error",
-  //                              __INFO__);
-  //   this->_serv_addr.sin_family = AF_INET;
-  //   this->_serv_addr.sin_port = htons(_port);
-  //   // Convert IPv4 and IPv6 addresses from text to binary form
-  //   auto test1 =
-  //       inet_pton(AF_INET, ip_address(_hostname).c_str(),
-  //       &_serv_addr.sin_addr);
-  //   SocketException::assertLTZ(test1, "IPv6 addresses not supported",
-  //   __INFO__); _proxy = new Socket(this->_socket); auto test2 =
-  //   ::connect(this->_socket, (struct sockaddr *)&_serv_addr,
-  //                          sizeof(_serv_addr));
-  //   SocketException::assertLTZ(test2, "Connection Failed", __INFO__);
-  // }
+      : _hostname(hostname) {
+    this->_socket = socket(AF_INET, SOCK_STREAM, 0);
+    SocketException::assertLTZ(_socket, "SocketClient creation error",
+                               __INFO__);
+    this->_serv_addr.sin_family = AF_INET;
+    this->_serv_addr.sin_port = htons(port);
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    auto test =
+        inet_pton(AF_INET, ip_address(_hostname).c_str(), &_serv_addr.sin_addr);
+    SocketException::assertLTZ(test, "IPv6 addresses not supported", __INFO__);
+    _proxy = new Socket(this->_socket);
+  }
 
   void SocketClient::connect() {
-    if ((_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-      printf("\n Socket creation error \n");
-      throw -1;
-    }
-
-    this->_serv_addr.sin_family = AF_INET;
-    this->_serv_addr.sin_port = htons(_port);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    auto _hn = ip_address(_hostname).c_str();
-    if (inet_pton(AF_INET, "159.223.103.27", &this->_serv_addr.sin_addr) <= 0) {
-      printf("\nInvalid address/ Address not supported \n");
-      throw -1;
-    }
-
-    if (::connect(_socket, (struct sockaddr *)&this->_serv_addr,
-                  sizeof(this->_serv_addr)) < 0) {
-      printf("\nConnection Failed \n");
-      throw -1;
-    }
-
-    _proxy = new Socket(this->_socket);
+    auto test = ::connect(this->_socket, (struct sockaddr *)&_serv_addr,
+                          sizeof(_serv_addr));
+    SocketException::assertLTZ(test, "Connection Failed", __INFO__);
   }
 
   std::string SocketClient::ip_address(const std::string &domainname) {
