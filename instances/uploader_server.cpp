0970b555596dda3799ff916b84b6c6d2b186822a
Perry Anderson <perry.anderson@gmail.com>
2021-10-26 02:47:05 -0700

    Refactored

--- instances/uploader_server.cpp
+++ instances/uploader_server.cpp
@@ -3,79 +3,40 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#define SIZE 1024 * 256
 
-void write_file(int sockfd) {
-  int n;
-  FILE *fp;
-  const char *filename = "recv.txt";
-  char buffer[SIZE];
-
-  fp = fopen(filename, "w");
-  while (1) {
-    n = recv(sockfd, buffer, SIZE, 0);
-    if (n <= 0) {
-      break;
-      return;
-    }
-    fprintf(fp, "%s", buffer);
-    bzero(buffer, SIZE);
-  }
-  return;
-}
+#include <extras/uploader/UploaderInterface.hpp>
 
 int main(int argc, char const *argv[]) {
+  //
+  // collect parameters
+  //
   if (argc < 2) {
     printf("need an ip\n");
     return -1;
   }
   const char *ip = argv[1];
   int port = 8080;
-  int e;
 
-  int sockfd, new_sock;
-  struct sockaddr_in server_addr, new_addr;
-  socklen_t addr_size;
-  // char buffer[SIZE];
+  //
+  // make connection
+  //
+  int sockfd;
+  struct sockaddr_in server_addr;
+  sockfd = configure_serversocket(ip, port, server_addr);
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) {
-    perror("[-]Error in socket");
-    exit(1);
-  }
-  printf("[+]Server socket created successfully.\n");
+  struct sockaddr_in new_addr;
+  socklen_t addr_size = sizeof(new_addr);
+  int new_sock = accept(sockfd, (struct sockaddr *)&new_addr, &addr_size);
 
-  // Forcefully attaching socket to the port 8080
-  int opt = 1;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
-                 sizeof(opt))) {
-    perror("setsockopt");
-    exit(EXIT_FAILURE);
-  }
-
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_port = port;
-  server_addr.sin_addr.s_addr = inet_addr(ip);
-
-  e = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-  if (e < 0) {
-    perror("[-]Error in bind");
-    exit(1);
-  }
-  printf("[+]Binding successfull.\n");
-
-  if (listen(sockfd, 10) == 0) {
-    printf("[+]Listening....\n");
-  } else {
-    perror("[-]Error in listening");
-    exit(1);
-  }
-
-  addr_size = sizeof(new_addr);
-  new_sock = accept(sockfd, (struct sockaddr *)&new_addr, &addr_size);
+  //
+  // do business
+  //
   write_file(new_sock);
   printf("[+]Data written in the file successfully.\n");
 
+  //
+  // close connection
+  //
   close(new_sock);
   close(sockfd);
 
