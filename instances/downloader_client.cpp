b51a7adba862ced3874edd1d57ef97d404431ef5
Perry Anderson <perry.anderson@gmail.com>
2021-10-26 02:30:45 -0700

    Documented code

--- instances/downloader_client.cpp
+++ instances/downloader_client.cpp
@@ -7,6 +7,9 @@
 #include <extras/uploader/UploaderInterface.hpp>
 
 int main(int argc, char const *argv[]) {
+  //
+  // collect parameters
+  //
   if (argc < 2) {
     printf("need an ip\n");
     return -1;
@@ -14,19 +17,21 @@ int main(int argc, char const *argv[]) {
   const char *ip = argv[1];
   int port = 8080;
 
+  //
+  // make connection
+  //
   struct sockaddr_in server_addr;
   int sockfd = connect_to_server(ip, port, server_addr);
-  //
-  //
-  //
 
+  //
+  // do business
+  //
   write_file(sockfd);
   printf("[+]Data written in the file successfully.\n");
 
   //
+  // close connection
   //
-  //
-
   printf("[+]Closing the connection.\n");
   close(sockfd);
 
--- instances/downloader_server.cpp
+++ instances/downloader_server.cpp
@@ -7,6 +7,9 @@
 #include <extras/uploader/UploaderInterface.hpp>
 
 int main(int argc, char const *argv[]) {
+  //
+  // collect parameters
+  //
   if (argc < 2) {
     printf("need an ip\n");
     return -1;
@@ -14,6 +17,9 @@ int main(int argc, char const *argv[]) {
   const char *ip = argv[1];
   int port = 8080;
 
+  //
+  // make connection
+  //
   int sockfd;
   struct sockaddr_in server_addr;
   sockfd = configure_serversocket(ip, port, server_addr);
@@ -23,24 +29,22 @@ int main(int argc, char const *argv[]) {
   int new_sock = accept(sockfd, (struct sockaddr *)&new_addr, &addr_size);
 
   //
+  // do business
   //
-  //
-
   const char *filename = "send.txt";
   FILE *fp = fopen(filename, "r");
   if (fp == NULL) {
     perror("[-]Error in reading file.");
     exit(1);
   }
-
   send_file(fp, new_sock);
   printf("[+]File data sent successfully.\n");
-  close(new_sock);
-  close(sockfd);
 
   //
+  // close connection
   //
-  //
+  close(new_sock);
+  close(sockfd);
 
   return 0;
 }
