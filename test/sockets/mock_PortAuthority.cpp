dd9087ac84d54469b58b91cecad0f0ac128ffa10
Perry Anderson <perry.anderson@gmail.com>
2021-10-26 17:42:12 -0700

    Added docs

--- test/sockets/mock_PortAuthority.cpp
+++ test/sockets/mock_PortAuthority.cpp
@@ -13,16 +13,26 @@ using namespace extras;
 using namespace fakeit;
 
 /**
- * @brief Mock FakeIt AlwaysDo demonstration
+ * @brief PortAuthorityInterface
+ * @ref https://eval.atlassian.net/browse/PE-16
+ *
+ * GIVEN we need to anticipate many requests to the a sockets based server WHEN
+ * we use a centralized port for the purposes of just allocating available
+ * socket ports THEN we can service many socket server requests from the same
+ * linux box, (local, Internet, VPN or otherwise)
  *
  */
-SCENARIO("Mock PortAuthorityInterface: domainName",
-         "[PortAuthorityInterface]") {
-  std::string correct_answer = "cplusplus.org";
+
+SCENARIO("Mock PortAuthorityInterface", "[PortAuthorityInterface]") {
+  std::string correct_domainName = "cplusplus.org";
+  int correct_request = 9000;
   Mock<PortAuthorityInterface> mock;
-  When(Method(mock, domainName)).Return(correct_answer);
+  When(Method(mock, domainName)).Return(correct_domainName);
+  When(Method(mock, request)).Return(correct_request);
 
   PortAuthorityInterface &i = mock.get();
-  REQUIRE(i.domainName() == correct_answer);
+  REQUIRE(i.domainName() == correct_domainName);
+  REQUIRE(i.request() == correct_request);
   Verify(Method(mock, domainName));
+  Verify(Method(mock, request));
 }
