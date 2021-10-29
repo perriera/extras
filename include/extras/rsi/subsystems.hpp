04e5e58b4852c3a0d4309bf8b42584ff12ad452a
Perry Anderson <perry.anderson@gmail.com>
2021-10-29 02:04:32 -0700

    Refactored

--- include/extras/rsi/subsystems.hpp
+++ include/extras/rsi/subsystems.hpp
@@ -11,44 +11,38 @@
 #include <string>
 
 namespace extras {
+
+  /**
+   * @brief rsi: (remote services interface)
+   *
+   * The following code has been plagerized from other authors
+   * and placed here, (where is/as is) as it is code that works
+   * and serves a platform for future work when it comes to
+   * uploading files over an IP:port connection.
+   *
+   */
   namespace rsi {
+
     void send_file(FILE *fp, int sockfd);
     void write_file(const char *filename, int sockfd);
+
     int connect_to_server(const char *ip, int port,
                           struct sockaddr_in &server_addr);
     int configure_serversocket(const char *ip, int port,
                                struct sockaddr_in &server_addr,
                                bool timeout = true);
-    void send_string(const std::string &msg, int sockfd);
+
     std::string read_string(int sockfd);
     int read_int(int sockfd);
     std::string read_line(int sockfd);
+
+    void send_string(const std::string &msg, int sockfd);
     void send_int(int msg, int sockfd);
     void send_line(const std::string &msg, int sockfd);
+
     static constexpr int const &SIZE = 1024 * 256;
+
   }  // namespace rsi
-  /**
-   * @brief UploaderInterface
-   *
-   * Used by the ChessMind project, it introduces an interface
-   * to safely convert a number to and from octal format.
-   *
-   */
-
-  interface FileSendWriteInterface {
-    virtual void write_file(int sockfd) pure;
-    virtual void send_file(FILE *fp, int sockfd) pure;
-  };
-
-  interface FileUploaderInterface extends FileSendWriteInterface {
-    // virtual void write_file(int sockfd) pure;
-    // virtual void send_file(FILE *fp, int sockfd) pure;
-  };
-
-  interface FileDownloaderInterface extends FileSendWriteInterface {
-    // virtual void write_file(int sockfd) pure;
-    // virtual void send_file(FILE *fp, int sockfd) pure;
-  };
 
 }  // namespace extras
 
