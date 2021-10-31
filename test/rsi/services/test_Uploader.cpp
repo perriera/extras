3501d136f8c884d9d8401621b5d7ae12b7c76fef
Perry Anderson <perry.anderson@gmail.com>
2021-10-31 16:47:51 -0700

    Added Test UploaderInterface

--- test/rsi/services/test_Uploader.cpp
+++ test/rsi/services/test_Uploader.cpp
@@ -1,9 +1,22 @@
 #include <extras/rsi/services/Uploader.hpp>
 #include <extras/strings.hpp>
+#include <filesystem>
 #include <iostream>
 
 #include "../../vendor/catch.hpp"
 #include "../../vendor/fakeit.hpp"
 
 using namespace extras;
-using namespace fakeit;
+namespace fs = std::filesystem;
+
+SCENARIO("Test UploaderInterface", "[UploaderInterface]") {
+  std::string target = "send_downloaded.txt";
+  if (fs::exists(target)) fs::remove(target);
+  REQUIRE(fs::exists("send.txt"));
+  REQUIRE(!fs::exists("send_downloaded.txt"));
+  system("build/uploader_server send_downloaded.txt 127.0.0.1 9000 &");
+  system("build/uploader_client send.txt 127.0.0.1 9000");
+  REQUIRE(fs::exists("send_downloaded.txt"));
+  REQUIRE(fs::exists("send.txt"));
+  if (fs::exists(target)) fs::remove(target);
+}
