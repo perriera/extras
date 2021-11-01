78c550eb541d9ed1c78898ce8fea89e26aa4e12e
Perry Anderson <perry.anderson@gmail.com>
2021-10-31 17:06:39 -0700

    Removed unused test case

--- test/rsi/services/mock_Uploader.cpp
+++ test/rsi/services/mock_Uploader.cpp
@@ -12,47 +12,7 @@ using namespace fakeit;
 // ss >> prg >> filename >> ip >> port;
 //
 
-SCENARIO("Mock UploaderInterface: client", "[UploaderInterface]") {
-  rsi::Parameter program = "build/rsi_client";
-  rsi::Parameter filename = "send.txt";
-  rsi::Parameter ip = "159.223.103.27";
-  rsi::Parameter port = "8080";
-  int socket = 5;
-  const char* argv[] = {program.c_str(), filename.c_str(), ip.c_str(),
-                        port.c_str()};
-  int argc = 4;
-  Mock<rsi::UploaderInterface> mock;
-  When(Method(mock, parameters)).AlwaysDo([](int argc, char const* argv[]) {
-    rsi::Parameters result;
-    for (int i = 0; i < argc; i++) result.push_back(argv[i]);
-    return result;
-  });
-  When(Method(mock, program)).Return(program);
-  When(Method(mock, filename)).Return(filename);
-  When(Method(mock, ip)).Return(ip);
-  When(Method(mock, port)).Return(port);
-  When(Method(mock, socket)).Return(socket);
-  When(Method(mock, upload)).Return();
-  When(Method(mock, close)).Return();
-
-  rsi::UploaderInterface& i = mock.get();
-  REQUIRE(i.parameters(argc, argv).size() == argc);
-  REQUIRE(i.program() == program);
-  REQUIRE(i.ip() == ip);
-  REQUIRE(i.port() == port);
-  REQUIRE(i.socket() == socket);
-  i.upload();
-  i.close();
-  Verify(Method(mock, parameters));
-  Verify(Method(mock, program));
-  Verify(Method(mock, ip));
-  Verify(Method(mock, port));
-  Verify(Method(mock, socket));
-  Verify(Method(mock, upload));
-  Verify(Method(mock, close));
-}
-
-SCENARIO("Mock UploaderInterface: server", "[UploaderInterface]") {
+SCENARIO("Mock UploaderInterface", "[UploaderInterface]") {
   rsi::Parameter program = "build/rsi_client";
   rsi::Parameter filename = "send.txt";
   rsi::Parameter ip = "159.223.103.27";
