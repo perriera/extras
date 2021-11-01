0e17ac43163b8b97ca0577cff9ccd2146e738457
Perry Anderson <perry.anderson@gmail.com>
2021-11-01 14:50:25 -0700

    Added test case

--- test/rsi/services/mock_Process.cpp
+++ test/rsi/services/mock_Process.cpp
@@ -14,14 +14,32 @@ using namespace fakeit;
 //
 
 SCENARIO("Mock ProcessInterface", "[ProcessInterface]") {
+  std::string filename_a = "send_a.txt";
+  std::string filename_b = "send_b.txt";
+  std::string filename_c = "send_c.txt";
   Mock<rsi::ProcessInterface> mock;
-  When(Method(mock, upload)).Return();
-  When(Method(mock, process)).Return();
-  When(Method(mock, download)).Return();
+  When(Method(mock, upload))
+      .AlwaysDo([&filename_a, &filename_b, &filename_c]() {
+        filename_b = filename_a;
+      });
+  When(Method(mock, process))
+      .AlwaysDo([&filename_a, &filename_b, &filename_c]() {
+        filename_c = filename_b;
+      });
+  When(Method(mock, download))
+      .AlwaysDo([&filename_a, &filename_b, &filename_c]() {
+        filename_a = filename_c;
+      });
   rsi::ProcessInterface& i = mock.get();
+  REQUIRE(filename_a == "send_a.txt");
+  REQUIRE(filename_b == "send_b.txt");
+  REQUIRE(filename_c == "send_c.txt");
   i.upload();
   i.process();
   i.download();
+  REQUIRE(filename_a == "send_a.txt");
+  REQUIRE(filename_b == "send_a.txt");
+  REQUIRE(filename_c == "send_a.txt");
   Verify(Method(mock, upload));
   Verify(Method(mock, process));
   Verify(Method(mock, download));
