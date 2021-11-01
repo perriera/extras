46c97213c92a6455b824c8dff44dbb5bf541b119
Perry Anderson <perry.anderson@gmail.com>
2021-10-31 20:37:44 -0700

    Test case added

--- instances/prototypes/process_server.cpp
+++ instances/prototypes/process_server.cpp
@@ -12,12 +12,23 @@
 
 int main(int argc, char const* argv[]) {
   try {
+    // upload
     extras::rsi::UploaderServer uploader;
     uploader.parameters(argc, argv);
     uploader.connect();
     uploader.transfer();
-    printf("[+]File data received successfully.\n");
+    printf("[+]File data sent successfully.\n");
     uploader.close();
+    printf("[+]Closed the connection.\n");
+    // process
+    std::cout << "processed" << std::endl;
+    // download
+    extras::rsi::DownloaderServer downloader;
+    downloader.parameters(argc, argv);
+    downloader.connect();
+    downloader.transfer();
+    printf("[+]File data sent successfully.\n");
+    downloader.close();
     printf("[+]Closed the connection.\n");
     return 0;
   } catch (std::exception& ex) {
