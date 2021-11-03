b16ffee4fb40ccb527f713c3f60f83cfa0ba9b00
Perry Anderson <perry.anderson@gmail.com>
2021-11-02 17:14:33 -0700

    Added test case

--- include/extras/rsi/services/SocketPool.hpp
+++ include/extras/rsi/services/SocketPool.hpp
@@ -5,9 +5,12 @@
 #include <sys/socket.h>
 
 #include <extras/keywords.hpp>
+#include <extras/rsi/exceptions.hpp>
 #include <extras/rsi/interfaces.hpp>
 #include <iostream>
+#include <map>
 #include <sstream>
+#include <vector>
 
 namespace extras {
   namespace rsi {
@@ -19,18 +22,45 @@ namespace extras {
      * level a series of predefined tokens are given to this interface. If all
      * tokens are reconized then a port number is returned for each token.
      *
+     * The purpose of a token is to inform the SocketPool instance what type of
+     * socket service will be expected to be running on that port. Earlier
+     * prototypes to this just returned a single port number. However, it was
+     * soon realized that perhaps more than one port is required and to inform
+     * the socket pool manager in advance of what type of service to running on
+     * that port would be advantageous.
+     *
+     * This version only returns a port number, a more enhanced version would
+     * support IP addresses, (hence supporting socket pools over different IPs).
+     *
      */
 
     using Socket = int;
     using PortNumberPool = std::vector<PortNumber>;
     using SocketRequestType = std::string;
     using SocketRequestTypeList = std::vector<SocketRequestType>;
+    using SocketRequestTypeMap = std::map<PortNumber, SocketRequestType>;
 
     interface SocketPoolInterface {
       virtual SocketRequestTypeList types() const pure;
       virtual PortNumberPool request(
-          const PortNumber& portNumber,
-          const SocketRequestTypeList& requests) pure;
+          const PortNumber &portNumber,
+          const SocketRequestTypeList &requests) pure;
+      virtual SocketRequestTypeMap lastRequest() const pure;
+      virtual SocketRequestTypeMap startServices(
+          const SocketRequestTypeMap &map) const pure;
+    };
+
+    /**
+     * @brief SocketException
+     *
+     * To be thrown if either string or value supplied is out of range.
+     *
+     */
+    concrete class UnsupportedTokenException extends RSIException {
+     public:
+      UnsupportedTokenException(std::string msg, const WhereAmI &whereAmI)
+          : RSIException(msg.c_str(), whereAmI) {}
+      static void assertion(const std::string &msg, const WhereAmI &ref);
     };
 
   }  // namespace rsi
--- test/rsi/services/mock_SocketPool.cpp
+++ test/rsi/services/mock_SocketPool.cpp
@@ -12,7 +12,7 @@ using namespace fakeit;
 // ss >> prg >> filename >> ip >> port;
 //
 
-SCENARIO("Mock SocketPoolInterface", "[SocketPoolInterface]") {
+SCENARIO("Mock SocketPoolInterface: types, request", "[SocketPoolInterface]") {
   PortNumber serverPort = 8080;
   rsi::SocketRequestTypeList typesList = {"upload", "process", "download"};
   rsi::SocketRequestTypeList requestsList = {"upload", "download"};
@@ -35,4 +35,53 @@ SCENARIO("Mock SocketPoolInterface", "[SocketPoolInterface]") {
   REQUIRE(i.request(serverPort, requestsList) == correctList);
   Verify(Method(mock, types));
   Verify(Method(mock, request));
+}
+
+SCENARIO("Mock SocketPoolInterface: startServices", "[SocketPoolInterface]") {
+  PortNumber serverPort = 8080;
+  rsi::SocketRequestTypeList typesList = {"upload", "process", "download"};
+  rsi::SocketRequestTypeList requestsList = {"upload", "download"};
+  rsi::SocketRequestTypeList badRequestsList = {"upload", "apples"};
+  PortNumber nextPortNumber = 9000;
+  rsi::PortNumberPool correctList = {9000, 9001};
+  rsi::SocketRequestTypeMap lastRequestsMap;
+  Mock<rsi::SocketPoolInterface> mock;
+  When(Method(mock, types)).AlwaysDo([&typesList]() { return typesList; });
+  When(Method(mock, request))
+      .AlwaysDo(
+          [&typesList, &nextPortNumber, &lastRequestsMap](
+              const PortNumber&, const rsi::SocketRequestTypeList& requests) {
+            rsi::PortNumberPool ports;
+            for (auto request : requests) {
+              bool found = false;
+              for (auto type : typesList) {
+                if (request == type) {
+                  lastRequestsMap[nextPortNumber] = request;
+                  ports.push_back(nextPortNumber++);
+                  found = true;
+                }
+              }
+              if (!found)
+                throw rsi::UnsupportedTokenException(request, __INFO__);
+            }
+            return ports;
+          });
+  When(Method(mock, lastRequest)).AlwaysDo([&lastRequestsMap]() {
+    return lastRequestsMap;
+  });
+  When(Method(mock, startServices))
+      .AlwaysDo(
+          [&typesList](const rsi::SocketRequestTypeMap& map) { return map; });
+  rsi::SocketPoolInterface& i = mock.get();
+  REQUIRE(i.types() == typesList);
+  auto list = i.request(serverPort, requestsList);
+  REQUIRE(list == correctList);
+  REQUIRE_THROWS_AS(i.request(serverPort, badRequestsList),
+                    rsi::UnsupportedTokenException);
+  auto latestRequestsMap = i.lastRequest();
+  REQUIRE(i.startServices(latestRequestsMap) == latestRequestsMap);
+  Verify(Method(mock, types));
+  Verify(Method(mock, request));
+  Verify(Method(mock, lastRequest));
+  Verify(Method(mock, startServices));
 }
