63622013b7fc4c8ec732df2c68b592cb5d52f899
Perry Anderson <perry.anderson@gmail.com>
2021-11-02 19:45:04 -0700

    Added test case

--- test/rsi/services/mock_SocketPool.cpp
+++ test/rsi/services/mock_SocketPool.cpp
@@ -92,8 +92,10 @@ SCENARIO("Mock SocketPoolInterface: parameters", "[SocketPoolInterface]") {
   rsi::Parameter filename = "send.txt";
   rsi::Parameter ip = "159.223.103.27";
   rsi::Parameter port = "8080";
-  const char* argv[] = {program.c_str(), filename.c_str(), ip.c_str(),
-                        port.c_str()};
+  rsi::SocketRequestTypeList requestsList = {"upload", "download"};
+  const char* argv[] = {
+      program.c_str(), filename.c_str(),        ip.c_str(),
+      port.c_str(),    requestsList[0].c_str(), requestsList[1].c_str()};
   int argc = 4;
   Mock<rsi::SocketPoolInterface> mock;
   When(Method(mock, parameters)).AlwaysDo([](int argc, char const* argv[]) {
@@ -105,13 +107,16 @@ SCENARIO("Mock SocketPoolInterface: parameters", "[SocketPoolInterface]") {
   When(Method(mock, filename)).Return(filename);
   When(Method(mock, ip)).Return(ip);
   When(Method(mock, port)).Return(port);
+  When(Method(mock, requests)).Return(requestsList);
   rsi::SocketPoolInterface& i = mock.get();
   REQUIRE(i.parameters(argc, argv).size() == argc);
   REQUIRE(i.program() == program);
   REQUIRE(i.ip() == ip);
   REQUIRE(i.port() == port);
+  REQUIRE(i.requests() == requestsList);
   Verify(Method(mock, parameters));
   Verify(Method(mock, program));
   Verify(Method(mock, ip));
   Verify(Method(mock, port));
+  Verify(Method(mock, requests));
 }
