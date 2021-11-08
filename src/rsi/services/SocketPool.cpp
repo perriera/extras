050ac87829192ab49103d0666fe672712e1e133b
Perry Anderson <perry.anderson@gmail.com>
2021-11-08 10:33:11 -0800

    Refactored

--- src/rsi/services/SocketPool.cpp
+++ src/rsi/services/SocketPool.cpp
@@ -25,69 +25,6 @@ namespace extras {
   namespace rsi {
 
     /**
-     * @brief SocketPoolParametersInterface ostream
-     *
-     * @param out
-     * @param obj
-     * @return std::ostream&
-     */
-
-    std::ostream &operator<<(std::ostream &out,
-                             const SocketPoolParametersInterface &obj) {
-      out << obj.program() << ' ';
-      out << obj.ip() << ' ';
-      out << obj.port() << ' ';
-      out << obj.filename() << ' ';
-      for (auto request : obj.requests()) out << request << ' ';
-      return out;
-    }
-
-    /**
-     * @brief SocketPoolParametersInterface istream
-     *
-     * @param in
-     * @param obj
-     * @return std::istream&
-     */
-    std::istream &operator>>(std::istream &in,
-                             SocketPoolParametersInterface &obj) {
-      std::string program, ip, port, filename, request;
-      in >> program >> ip >> port >> filename;
-      SocketRequestTypeList requests;
-      while (in.good()) {
-        in >> request;
-        if (in.good()) requests.push_back(request);
-      }
-      obj.setProgram(program);
-      obj.setIP(ip);
-      obj.setPort(stoi(port));
-      obj.setFilename(filename);
-      obj.setRequests(requests);
-      return in;
-    }
-    /**
-     * @brief abstract class SocketPool
-     *
-     *   build/rsi_client 127.0.0.1 8080 transfer send.txt
-     *   ss >> prg >> filename >> ip >> port;
-     *
-     */
-    Parameters SocketPool::parameters(int argc, char const *argv[]) {
-      if (argc < 4) {
-        std::cout << "params: filename ip port" << std::endl;
-        throw RSIException("params: filename ip port", __INFO__);
-      }
-      Parameters copy;
-      std::stringstream ss;
-      for (int i = 0; i < argc; i++) {
-        ss << argv[i] << ' ';
-        copy.push_back(argv[i]);
-      }
-      ss >> *this;
-      return copy;
-    }
-
-    /**
      * @brief concrete class SocketPoolClient
      *
      *   build/rsi_client 127.0.0.1 8080 transfer send.txt
