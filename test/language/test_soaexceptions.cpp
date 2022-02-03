a6c15eaf6a5ee9dd894c4e83998088de12d0f4a5
Perry Anderson <perry.anderson@gmail.com>
2022-02-02 17:33:49 -0800

    Added soa_tests

---
+++ test/language/test_soaexceptions.cpp
@@ -0,0 +1,53 @@
+/**
+ * @brief the "MIT/X Consortium License", (adapted for EXPARX.COM)
+ *
+ * Copyright (C) November 22, 2021 EXPARX INCORPORATED
+ *
+ * Permission is hereby  granted,  free of charge,  to  any  person
+ * obtaining a copy of this software and  associated  documentation
+ * files   (the  "Software"),  to deal  in   the  Software  without
+ * restriction, including  without  limitation the rights  to  use,
+ * copy,  modify, merge,  publish,  distribute,  sublicense, and/or
+ * sell copies of the  Software, and to permit persons  to whom the
+ * Software  is  furnished to  do  so,  subject  to  the  following
+ * conditions:
+ *
+ * (See LICENSE.md for complete details)
+ *
+ */
+
+#include <extras/exceptions.hpp>
+#include <iostream>
+
+#include "../vendor/catch.hpp"
+#include "../vendor/fakeit.hpp"
+#include "extras/strings.hpp"
+
+//
+// https://github.com/eranpeer/FakeIt/wiki/Quickstart
+//
+
+using namespace extras;
+using namespace fakeit;
+
+/**
+ * @brief Mock ExceptionInterface
+ *
+ * We need to provide additional information for loggers
+ * as well as to streamline the custom exception process
+ * for supported applications.
+ *
+ */
+using namespace std;
+using namespace extras;
+
+SCENARIO("Test ExceptionInterface: soa whereiam",
+         "[soa_exceptions_testcases]") {
+  auto test1 = __INFO__;
+  auto test2 = __INFO__;
+  auto line = __LINE__;
+  REQUIRE(test1._file == test2._file);
+  REQUIRE(test1._func == test2._func);
+  REQUIRE(test1._line == test2._line - 1);
+  REQUIRE(test1._line == line - 2);
+}
