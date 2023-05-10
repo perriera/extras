22d323ed73eb747de7a694797699526c4b4871eb
Your Name <you@example.com>
2023-05-10 06:33:10 +0000

    Added v8.21.5

--- bin/tagit.sh
+++ bin/tagit.sh
@@ -4,7 +4,7 @@ if [ $# -eq 0 ]
     echo "No arguments supplied"
     echo Syntax: $0 tag comment1 comment2 ... comment9 
     echo tag should be major.minor.patch 
-    echo "for examole here's the last tag:"
+    echo "for example here's the last tag:"
     git describe --tags --abbrev=0 
     exit
 fi
